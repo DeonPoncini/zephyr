07dc8ccc1e882d31488e9515bbc0ec86767590a4
Deon Poncini <dex1337@gmail.com>
2014-09-09 19:37:37 -0700

    Add queueloop class
    Allows for passing a queue of data between threads

---
+++ include/util/QueueLoop.h
@@ -0,0 +1,94 @@
+#ifndef UTIL_QUEUELOOP_H
+#define UTIL_QUEUELOOP_H
+
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+
+namespace util
+{
+
+template <typename T>
+class QueueLoop
+{
+public:
+    QueueLoop();
+    ~QueueLoop();
+    bool shouldExit();
+    void terminate();
+
+    bool more() const;
+    void write(T t);
+    T read();
+
+private:
+    bool mExit;
+    bool mWaitVar;
+    std::deque<T> mQueue;
+    mutable std::mutex mReadMutex;
+    std::mutex mMutex;
+    std::condition_variable mCondVar;
+};
+
+template <typename T>
+QueueLoop<T>::QueueLoop() :
+    mExit(false),
+    mWaitVar(false)
+{
+}
+
+template <typename T>
+QueueLoop<T>::~QueueLoop()
+{
+    terminate();
+}
+
+template <typename T>
+bool QueueLoop<T>::shouldExit()
+{
+    std::unique_lock<std::mutex> l(mMutex);
+    mCondVar.wait(l,[&](){return mWaitVar;});
+    mWaitVar = false;
+    return mExit;
+}
+
+template <typename T>
+void QueueLoop<T>::terminate()
+{
+    mExit = true;
+    mWaitVar = true;
+    mCondVar.notify_all();
+}
+
+template <typename T>
+bool QueueLoop<T>::more() const
+{
+    std::unique_lock<std::mutex> l(mReadMutex);
+    return !mQueue.empty();
+}
+
+template <typename T>
+void QueueLoop<T>::write(T t)
+{
+    {
+        std::unique_lock<std::mutex> l(mReadMutex);
+        mQueue.push_back(std::move(t));
+    }
+    // notify the waiting thread to continue
+    mWaitVar = true;
+    mCondVar.notify_all();
+}
+
+template <typename T>
+T QueueLoop<T>::read()
+{
+    std::unique_lock<std::mutex> l(mReadMutex);
+    auto t = mQueue.front();
+    mQueue.pop_front();
+    return t;
+}
+
+
+}
+
+#endif
