474864639946f06abb90a5398c4a881714ac0c15
Deon Poncini <dex1337@gmail.com>
2014-12-27 15:09:40 -0800

    Add variable name to namespace binding

--- include/zephyr/CExport.h
+++ include/zephyr/CExport.h
@@ -2,6 +2,12 @@
 #define ZEPHYR_CEXPORT_H
 
 #ifdef __cplusplus
+#define Z_VAR(ns, n) n
+#else
+#define Z_VAR(ns, n) ns ## _ ## n
+#endif
+
+#ifdef __cplusplus
 #define Z_NS_START(n) namespace n {
 #define Z_NS_END }
 #else
@@ -12,19 +18,19 @@
 #ifdef __cplusplus
 #define Z_ENUM_CLASS(ns, n) enum class n
 #else
-#define Z_ENUM_CLASS(ns, n) enum ns ## _ ## n
+#define Z_ENUM_CLASS(ns, n) enum Z_VAR(ns, n)
 #endif
 
 #ifdef __cplusplus
 #define Z_ENUM(ns, n) enum n
 #else
-#define Z_ENUM(ns, n) enum ns ## _ ## n
+#define Z_ENUM(ns, n) enum Z_VAR(ns, n)
 #endif
 
 #ifdef __cplusplus
 #define Z_STRUCT(ns, n) struct n
 #else
-#define Z_STRUCT(ns, n) struct ns ## _ ## n
+#define Z_STRUCT(ns, n) struct Z_VAR(ns, n)
 #endif
 
 #endif
