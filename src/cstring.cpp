26c873c7273f9786b934c95840a150a94c07fa2c
Deon Poncini <dex1337@gmail.com>
2015-01-02 14:17:52 -0800

    Actually copy the c string
    Fix a bug where I wasn't copying the std string into the c buffer

--- src/cstring.cpp
+++ src/cstring.cpp
@@ -10,6 +10,7 @@ namespace cstring
 char* make_copy(const std::string& s)
 {
     auto c = new char[s.size() + 1];
+    memcpy(c, s.data(), s.size()*sizeof(char));
     c[s.size()] = '\0';
     return c;
 }
